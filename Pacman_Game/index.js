7aaef03a0c61ff2c88a058279d656e1e1fd8bf46
ak7662 <ak7662@gmail.com>
2021-04-14 23:45:35 +0530

    more progress in pacman game

---
+++ Pacman_Game/index.js
@@ -0,0 +1,232 @@
+const width = 28
+const grid = document.querySelector(".grid")
+const score = document.querySelector("#score")
+score.textContent = 0
+const newGridArray = []
+let currentScore = 0
+//28 * 28 = 784
+  // 0 - pac-dots
+  // 1 - wall
+  // 2 - ghost-lair
+  // 3 - power-pellet
+  // 4 - empty
+
+  const layout = [
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 
+]
+
+for(let i of layout){
+    const newGrid = document.createElement("div")
+    if(i == 0){
+        newGrid.classList.add("pac-dots")
+    }
+    else if(i == 1){
+        newGrid.classList.add("wall")
+    }
+    else if(i == 2){
+        newGrid.classList.add("ghost-lair")
+    }
+    else if(i == 3){
+        newGrid.classList.add("power-pellet")
+    }
+    else if(i == 4){
+        newGrid.classList.add("empty")
+    }
+    grid.appendChild(newGrid)
+    newGridArray.push(newGrid)
+}
+// console.log(newGridArray)
+
+let pacmanCurrentIndex = 500
+newGridArray[pacmanCurrentIndex].classList.add("pacman")
+//remove the pacman classList and then change cueent postion of pacman and then reassign the pacman classlist
+function control(e){
+    pacDotEaten()
+    powerPalletEaten()
+    switch(e.keyCode){
+        
+        case 37:
+            newGridArray[pacmanCurrentIndex].classList.remove("pacman")
+            if(!newGridArray[pacmanCurrentIndex-1].classList.contains("ghost-lair") && !newGridArray[pacmanCurrentIndex-1].classList.contains("wall") && pacmanCurrentIndex % width !== 0){
+                pacmanCurrentIndex--
+            }
+            
+            newGridArray[pacmanCurrentIndex].classList.add("pacman")
+            // newGridArray[pacmanCurrentIndex].classList.remove("pacman")
+
+            // if(pacmanCurrentIndex % width === 0 && newGridArray[pacmanCurrentIndex+(width-1)].classList.contains("empty")){
+            //     pacmanCurrentIndex = pacmanCurrentIndex+width-1
+            //     // console.log("hero")
+
+            // }
+            // newGridArray[pacmanCurrentIndex].classList.add("pacman")
+
+            // console.log("left")
+            break
+        case 39:
+            newGridArray[pacmanCurrentIndex].classList.remove("pacman")
+            if(!newGridArray[pacmanCurrentIndex+1].classList.contains("ghost-lair") && !newGridArray[pacmanCurrentIndex+1].classList.contains("wall") && pacmanCurrentIndex % width !== 27){
+                pacmanCurrentIndex++
+            }
+            newGridArray[pacmanCurrentIndex].classList.add("pacman")
+            // console.log("right")
+            break
+        case 38:
+            newGridArray[pacmanCurrentIndex].classList.remove("pacman")
+            if(!newGridArray[pacmanCurrentIndex-width].classList.contains("ghost-lair") && !newGridArray[pacmanCurrentIndex-width].classList.contains("wall") && pacmanCurrentIndex - width > 0){
+                pacmanCurrentIndex -= width 
+            }
+            newGridArray[pacmanCurrentIndex].classList.add("pacman")
+            // console.log("up")
+            break
+        case 40:
+            newGridArray[pacmanCurrentIndex].classList.remove("pacman")
+            if(!newGridArray[pacmanCurrentIndex+width].classList.contains("ghost-lair") && !newGridArray[pacmanCurrentIndex+width].classList.contains("wall") && pacmanCurrentIndex + width < width * width){
+                pacmanCurrentIndex += width 
+            }
+            newGridArray[pacmanCurrentIndex].classList.add("pacman")
+            // console.log("down")
+            break
+    }
+}
+
+document.addEventListener("keydown", control)
+
+function pacDotEaten(){
+    if(newGridArray[pacmanCurrentIndex].classList.contains("pac-dots")){
+        newGridArray[pacmanCurrentIndex].classList.remove("pac-dots")
+        currentScore++
+    }
+    score.textContent = currentScore
+
+}
+
+function powerPalletEaten(){
+    if(newGridArray[pacmanCurrentIndex].classList.contains("power-pellet")){
+        newGridArray[pacmanCurrentIndex].classList.remove("power-pellet")
+        currentScore+=10
+        score.textContent = currentScore
+        ghosts.forEach(ghost => {
+            ghost.isScared = true
+        })
+        setTimeout(() => {
+            ghosts.forEach(ghost => ghost.isScared = false)
+            }, 10000)
+
+    }
+}
+class Ghost{
+    constructor(className, startIndex, speed){
+        this.className = className
+        this.startIndex = startIndex
+        this.speed = speed
+        this.currentIndex = startIndex
+        this.isScared = false
+        this.timerId = NaN
+    }
+}
+
+const ghosts = [
+    new Ghost('blinky', 348, 250),
+    new Ghost('pinky', 376, 400),
+    new Ghost('inky', 351, 300),
+    new Ghost('clyde', 379, 500)
+
+]
+
+
+ghosts.forEach(ghost => {
+    newGridArray[ghost.startIndex].classList.add(ghost.className)
+    newGridArray[ghost.startIndex].classList.add("ghost")
+    })
+
+ghosts.forEach(ghost => moveGhost(ghost))
+function moveGhost(ghost){
+    console.log("mov ghost")
+    const directions = [-1, +1, +width, -width]
+    let direction = directions[Math.floor(Math.random()*directions.length)]
+    console.log(direction)
+
+    ghost.timerId = setInterval(() => {
+        if(!newGridArray[ghost.currentIndex+direction].classList.contains("ghost") && !newGridArray[ghost.currentIndex+direction].classList.contains("wall")){
+            newGridArray[ghost.currentIndex].classList.remove(ghost.className)
+            newGridArray[ghost.currentIndex].classList.remove("ghost")
+            
+            ghost.currentIndex += direction
+            newGridArray[ghost.currentIndex].classList.add(ghost.className)
+            newGridArray[ghost.currentIndex].classList.add("ghost")
+            
+        }else{
+            direction = directions[Math.floor(Math.random()*directions.length)]
+        }
+    },ghost.speed)
+}
+
+
+
+
+// function controlup(e){
+//     newGridArray[pacmanCurrentIndex].classList.remove("pacman")
+//             if(pacmanCurrentIndex - width > 0){
+//                 pacmanCurrentIndex -= width 
+//             }
+//             newGridArray[pacmanCurrentIndex].classList.add("pacman")
+//             // console.log("up")
+// }
+// function controlDown(e){
+//     newGridArray[pacmanCurrentIndex].classList.remove("pacman")
+//             if(pacmanCurrentIndex + width < width * width){
+//                 pacmanCurrentIndex += width 
+//             }
+//             newGridArray[pacmanCurrentIndex].classList.add("pacman")
+//             // console.log("down")
+// }
+// function controlRight(e){
+//     newGridArray[pacmanCurrentIndex].classList.remove("pacman")
+//             if(pacmanCurrentIndex % width !== width -1){
+//                 pacmanCurrentIndex++
+//             }
+//             newGridArray[pacmanCurrentIndex].classList.add("pacman")
+// }
+// function controlLeft(e){
+//     newGridArray[pacmanCurrentIndex].classList.remove("pacman")
+//             if(pacmanCurrentIndex % width !== 0){
+//                 pacmanCurrentIndex--
+//             }
+//             newGridArray[pacmanCurrentIndex].classList.add("pacman")
+// }
+// const clickUp = document.querySelector(".up")
+// const clickDown = document.querySelector(".down")
+// const clickRight = document.querySelector(".right")
+// const clickLeft = document.querySelector(".left")
+// clickUp.addEventListener("click",controlup)
+// clickDown.addEventListener("click",controlDown)
+// clickRight.addEventListener("click",controlRight)
+// clickLeft.addEventListener("click",controlLeft)
