0f750e3f4763dfa1aff4a68e73fb07eab9b6785e
ak7662 <ak7662@gmail.com>
2021-06-26 18:59:22 +0530

    SettigUpTheLayout

---
+++ SettigUpTheLayout/index.js
@@ -0,0 +1,17 @@
+// document.getElementById('action').addEventListener("click", ()=>{
+    
+//     // call user API
+//     // store random first name in a variable firstUser
+//     // call user API again, after you finish calling first one
+//     // store random first name in variable secondUser 
+//     // console log `${firstUser} and ${secondUser} are friends`
+    
+//     const userApi = 'https://randomuser.me/api/'
+//     // const response = await fetch(userApi)
+//     // const data = response.json()
+//     console.log("data")
+//     })
+
+document.getElementById("ul").addEventListener("click", () => console.log("hello"))
+    
+    
